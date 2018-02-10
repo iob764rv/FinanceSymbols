884a26a4396e4c69dc6e9f22fcc8b7ed25805b3a
iob764rv <iic287m@gmail.com>
2018-02-09 19:38:22 -0500

    Update FinanceSymbols.java

--- src/FinanceSymbols.java
+++ src/FinanceSymbols.java
@@ -1,7 +1,7 @@
 import com.jimmoores.quandl.DataSetRequest;
 import com.jimmoores.quandl.MetaDataResult;
 import com.jimmoores.quandl.TabularResult;
-import com.jimmoores.quandl.SearchResult;
+
 import com.jimmoores.quandl.classic.ClassicQuandlSession;
 import java.net.URL;
 import java.net.URLConnection;
