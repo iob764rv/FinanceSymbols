4beeeff5a69fb83ecf315a97be012da5bac34ef4
iob764rv <iic287m@gmail.com>
2018-04-26 21:42:31 -0400

    Update AsyncReaderWrapper.java

--- src/AsyncReaderWrapper.java
+++ src/AsyncReaderWrapper.java
@@ -5,8 +5,7 @@ import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonParser.NumberType;
+
 import com.fasterxml.jackson.core.JsonToken;
 
 public abstract class AsyncReaderWrapper
